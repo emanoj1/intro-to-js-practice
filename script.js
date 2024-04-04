fd15c87e8ddb07ad1184b3c610411969f1da7de2
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-04-04 20:54:13 +1100

    feat: add JS example of datatypes

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-console.log("hello from the script.js file!");
+// Outout variable
+let world = "Earth"
+
+console.log(`Hello ${world}, I am Manoj!`);
+console.log ("Hello " + "world!")
+
+// Arrays
+let favouriteFruits = [
+    "mango",
+    "pear",
+    "papaya"
+];
+
+console.log(favouriteFruits)
+
+favouriteFruits.forEach(fruit =>{
+    console.log("I love " + fruit)
+});
+
+// Nested Array
+let person = {
+    name: "manoj",
+    codingLanguage: ["Python", "Javascript"],
+    hobbies: [
+        {
+            name: "vlogs",
+            skill: 10,
+        },
+        {
+            name: "sketching",
+            skill: 10,
+        }
+    ] 
+}
+
+console.log(person)
+
+// Matchcase
+let weather = "sunny";
+
+switch (weather) {
+    case "sunny":
+        console.log("sunny is nice")
+        break;
+    case "rainy":
+        console.log("100 ml today!")
+        break;
+    default:
+        console.log("Oh that weather")
+        break;
+}
